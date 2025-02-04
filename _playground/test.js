4c723f8711bf549e9e87cac3fa19464b5a01c303
JnJ One <jnjsoft.one@gmail.com>
2025-02-04 19:03:18 +0900

    test

--- _playground/test.js
+++ _playground/test.js
@@ -29,7 +29,7 @@ const fetchByChrome = async (url, { email = GOOGLE_EMAIL, userDataDir = CHROME_U
       '--disable-notifications',
       '--disable-infobars',
       '--ignore-certificate-errors',
-      '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'  // 최신 Chrome 유저 에이전트
+      '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', // 최신 Chrome 유저 에이전트
     ],
   });
 
@@ -52,16 +52,16 @@ const fetchByChrome = async (url, { email = GOOGLE_EMAIL, userDataDir = CHROME_U
 
     return {
       title: title.substring(0, 100),
-      content
+      content,
     };
   } finally {
     await chrome.close();
   }
 };
 
-let url = "https://chatgpt.com/c/6783a538-b5cc-8012-ab29-476795b18a44"  // 챗gpt
-url = "https://www.genspark.ai/agents?id=1059b776-f712-4a23-8fe9-0059bbaeda27" // 젠스파크
+let url = 'https://chatgpt.com/c/6783a538-b5cc-8012-ab29-476795b18a44'; // 챗gpt
+// url = "https://www.genspark.ai/agents?id=1059b776-f712-4a23-8fe9-0059bbaeda27" // 젠스파크
 // url = "https://claude.ai/chat/24741e4f-52f2-4998-9e3f-6a7d33ebe06e" // 클로드
 // url = "https://www.perplexity.ai/search/nodejsreul-sayonghayeo-appeul-XbV7QP0cSAmOWMH9TqXo3Q" // 퍼플릭시티
 
-await fetchByChrome(url, {email: "bigwhitekmc@gmail.com"}).then(console.log);
+await fetchByChrome(url, { email: 'bigwhitekmc@gmail.com' }).then(console.log);
